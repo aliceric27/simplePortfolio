ff0a6c3a18ff6ec0c4b93eeed3ff7f6b96685841
alexhuang221 <aliceric27@gmail.com>
2024-11-01 16:58:59 +0800

    feat: add random pic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 const { createApp } = Vue
 
+function getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
 createApp({
     data() {
         return {
@@ -9,42 +13,42 @@ createApp({
                     id: 1,
                     title: "專案一",
                     description: "使用 Vue.js 開發的網頁應用",
-                    image: "https://picsum.photos/300/200?random=1",
+                    image: `https://picsum.photos/300/200?random=${getRandomNumber()}`,
                     link: "#"
                 },
                 {
                     id: 2,
                     title: "專案二",
                     description: "響應式電商網站設計",
-                    image: "https://picsum.photos/300/200?random=2",
+                    image: `https://picsum.photos/300/200?random=${getRandomNumber()}`,
                     link: "#"
                 },
                 {
                     id: 3,
                     title: "專案三",
                     description: "社群媒體儀表板",
-                    image: "https://picsum.photos/300/200?random=3",
+                    image: `https://picsum.photos/300/200?random=${getRandomNumber()}`,
                     link: "#"
                 },
                 {
                     id: 4,
                     title: "專案四",
                     description: "使用 Vue.js 開發的網頁應用",
-                    image: "https://picsum.photos/300/200?random=1",
+                    image: `https://picsum.photos/300/200?random=${getRandomNumber()}`,
                     link: "#"
                 },
                 {
                     id: 5,
                     title: "專案五",
                     description: "響應式電商網站設計",
-                    image: "https://picsum.photos/300/200?random=2",
+                    image: `https://picsum.photos/300/200?random=${getRandomNumber()}`,
                     link: "#"
                 },
                 {
                     id: 6,
                     title: "���案六",
                     description: "社群媒體儀表板",
-                    image: "https://picsum.photos/300/200?random=3",
+                    image: `https://picsum.photos/300/200?random=${getRandomNumber()}`,
                     link: "#"
                 }
             ]
